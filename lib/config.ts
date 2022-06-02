baa840d728133aa16e41f48874c8fde32911d8e5
tianhang <tianhang.shi@thoughtworks.com>
2022-06-02 21:29:22 +0800

    Revert "fix:refactor code."
    This reverts commit 30f0cac8915de80c4b34a37d21d82763ddaf78df.

--- lib/config.ts
+++ lib/config.ts
@@ -129,7 +129,7 @@ export const apiBaseUrl = `/api`
 
 export const api = {
   searchNotion: `${apiBaseUrl}/search-notion`,
-  getSocialImage: `${apiBaseUrl}/social-image`
+  getSocialImage: `${apiBaseUrl}/image/social-image`
 }
 
 // ----------------------------------------------------------------------------
