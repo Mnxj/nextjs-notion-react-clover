f6a209ad12b87c94f3bda6a68b049d955765b8df
tianhang <tianhang.shi@thoughtworks.com>
2024-05-18 19:23:02 +0800

    [clover] fix: use fs exchange redis.

--- components/writeJson.tsx
+++ components/writeJson.tsx
@@ -10,12 +10,7 @@ export const writeJson = (filename: string,value:any) => {
 }
 
 export const appendWriteJson = (filename: string,value:any) => {
-    fs.appendFile(filename, JSON.stringify(value),err => {
-        if(err){
-            console.log('写入失败')
-            return;
-        }
-    });
+    fs.writeFileSync(filename, JSON.stringify(value));
 }
 
 const readJson = (filename: string) =>  JSON.parse(fs.readFileSync(filename, 'utf8'));
